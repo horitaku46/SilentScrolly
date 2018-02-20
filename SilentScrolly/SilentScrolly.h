f7da5f289b9312dde4e45aa082c64c7f31a15360
horitaku46 <horitaku46@gmail.com>
2018-02-20 12:35:59 +0900

    Add Example

---
+++ SilentScrolly/SilentScrolly.h
@@ -0,0 +1,19 @@
+//
+//  SilentScrolly.h
+//  SilentScrolly
+//
+//  Created by Takuma Horiuchi on 2018/02/19.
+//  Copyright © 2018年 Takuma Horiuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SilentScrolly.
+FOUNDATION_EXPORT double SilentScrollyVersionNumber;
+
+//! Project version string for SilentScrolly.
+FOUNDATION_EXPORT const unsigned char SilentScrollyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SilentScrolly/PublicHeader.h>
+
+
